7b27e611ea0716a2c787021cbdf76a5123eea46b
Barbara Compagnoni <bcphotodesigns@aol.com>
2022-01-21 18:57:14 -0500

    update

---
+++ studentWork/Barb/madlibs.py
@@ -0,0 +1,28 @@
+# Challenge Madlibs
+
+# Storing user input in variables
+num1 = input("Please enter a number.")
+noun1 = input("Please enter a noun (plural).")
+verb1 = input("Please enter a verb.")
+adj1 = input("Please enter an adjective.")
+num2 = input("Please enter a number.")
+firstName1 = input("Please enter a name.")
+firstName2 = input("Please enter another name.")
+verbed1 = input("Please enter a verb (past tense).")
+noun2 = input("Please enter a place.")
+verbed2 = input("Please enter verb (past tense).")
+num1 = input("Please enter a number.")
+adj2 = input("Please enter an adjective.")
+adj3 = input("Please enter an other adjective.")
+verbed3 = input("Please enter a verb (past tense).")
+verbed4 = input("Please enter a another verb (past tense).")
+noun3 = input("Please enter a noun.")
+verbed5 = input("Please enter a verb (past tense).")
+noun4 = input("Please enter a noun (plural).")
+verb2 = input("Please enter a verb.")
+adverb1 = input("Please enter an adverb.")
+num3 = input("Please enter a number.")
+
+madLib = "The Moon Landing -- On July " + num1 + " 1969, two American " + noun1 + " were the first to " + verb1 + " on the moon. This " + adj1 + " trip took " + num2 + " days to reach the moon from " + noun2 + ". As " + firstName1 + " Armstrong and " + firstName2 + " Aldrin " + verbed1 + " onto the " + noun2 + " of the moon, Armstrong " + verbed2 + " the famous words, 'That's' " + num1 + " step for a man, one " + adj3 + " leap for mankind'. Soon after, Aldrin " + verbed3 + " onto the moon and together, they " + verbed4 + " a U.S. " + noun3 + " on the surface. They " + verbed5 + " " + noun4 + " from the moon's surface to " + verb2 + " back to Earth and " + adverb1 + " returned home " + num3 + " days later. "
+
+print(madLib)

211c9aac1f00978b1dcbaa4ff180abd6b0fb5654
Barbara Compagnoni <bcphotodesigns@aol.com>
2022-01-22 08:42:35 -0500

    Add files via upload

---
+++ studentWork/Arjit Nair/MadLibs.py
@@ -0,0 +1,27 @@
+#MadLib Game
+#input() = take input from the user
+
+#Madlib Game
+#input() - take input from the user
+
+number1 = input("Please enter a number.")
+print(number1)
+noun1 = ""
+verb1 = ""
+adj1 = ""
+number2 = ""
+place1 = ""
+firstName1 = ""
+firstName2 = ""
+verbEd1 = ""
+noun2 = ""
+adj2 = ""
+adj3 = ""
+verbEd2 = ""
+verbEd3 = ""
+noun3 = ""
+verbEd4 = ""
+pluralNoun1 = ""
+verb2 = ""
+adverb1 = ""
+number2 = ""
---
+++ studentWork/Tristian Pagano/madlibisyes.py
@@ -0,0 +1,19 @@
+#Madlib Game
+#input = take input from user
+
+number1 = input("Please enter a number")
+noun1 = ""
+verb1 = ""
+number2 = ""
+place = ""
+firstname1 = ""
+firstname2 = ""
+verb2 = ""
+verb3 = ""
+noun2 = ""
+verb4 = ""
+number3 = ""
+verb5 = ""
+adjective = ""
+
+print (number1)
